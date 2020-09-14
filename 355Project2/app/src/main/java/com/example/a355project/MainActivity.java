953e6ca6f5020978459964983dec6d2cd70d4d72
Jesus Guerrero <guerreroj3@mymail.vcu.edu>
2020-09-14 13:32:00 -0400

    Fixed the bad commit

--- 355Project2/app/src/main/java/com/example/a355project/MainActivity.java
+++ 355Project2/app/src/main/java/com/example/a355project/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
 
     public static void main(String[] args){
 
-        System.out.prinln("Hello World");
+        System.out.println("Hello World");
     }
 }
