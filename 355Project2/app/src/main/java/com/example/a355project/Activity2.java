81d79cbf0ee5480b6302f2af4c69be7e15f71350
Bethel Desisa <>
2020-09-18 23:14:51 -0400

    Testing out spinner object to create a drop down button for food type selection when Activity2 is clicked.

--- 355Project2/app/src/main/java/com/example/a355project/Activity2.java
+++ 355Project2/app/src/main/java/com/example/a355project/Activity2.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Activity2 extends AppCompatActivity {
+public class Activity2 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Button button2;
 
@@ -23,10 +27,32 @@ public class Activity2 extends AppCompatActivity {
                 openActivityMain();
             }
         });
+    /*
+    Spinner object creates the dropdown button for "Type of Food"
+    */
+        Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.TypeOfFood, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
     }
 
     public void openActivityMain(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
