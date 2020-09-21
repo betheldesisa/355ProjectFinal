429ae04ead6c7a4cf6d502fb2c2d3d200421221e
Jesus Guerrero <guerreroj3@mymail.vcu.edu>
2020-09-21 15:33:19 -0400

    started working on slider value text. Still in working progress. It should not crash the app, let me know if it does.

--- 355Project2/app/src/main/java/com/example/a355project/Activity2.java
+++ 355Project2/app/src/main/java/com/example/a355project/Activity2.java
@@ -8,10 +8,47 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    /**************************** WORKING ON SLIDER BAR VALUE *********************/
+    private static SeekBar seek_bar;
+    private static TextView sliderValue;
+
+    public void seekbar() {
+        seek_bar = (SeekBar)findViewById(R.id.seekBar);
+        sliderValue = (TextView)findViewById(R.id.sliderValue);
+        sliderValue.setText(seek_bar.getProgress());
+
+        seek_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            int progressValue;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                progressValue = progress;
+                sliderValue.setText(progress);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                sliderValue.setText(progressValue);
+            }
+        });
+    }
+    /********* SLIDER BAR VALUE STUFF ***********************************************************/
+
+
 
     private Button button2;
 
@@ -19,6 +56,10 @@ public class Activity2 extends AppCompatActivity implements AdapterView.OnItemSe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        /************** Need to fix this for slider bar value *********/
+        //seekbar();
+        /*********** Slider bar VALUE STUFF ************/
 
         button2 = (Button) findViewById(R.id.button);
         button2.setOnClickListener(new View.OnClickListener() {
